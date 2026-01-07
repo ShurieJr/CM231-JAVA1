56738bd81262449b81345d2fee4dd1eb1f63ff64
ShurieJr <shuuriyejr@gmail.com>
2026-01-07 09:16:42 +0300

    arraysDemo

---
+++ src/Arrays/arraysDemo.java
@@ -0,0 +1,17 @@
+package Arrays;
+
+public class arraysDemo {
+    static void main() {
+        //declaring array , creating array
+//        String[] names ;
+//        names = new String[5];
+        String[] names = new String[3];
+        int[] marks = {50 , 40 , 90};
+//        marks[0] = 50;
+//        marks[1] = 40;
+//        marks[2] = marks[0] + marks[1];
+        System.out.println(marks[0]);
+        System.out.println(marks[1]);
+        System.out.println(marks[2]);
+    }
+}
